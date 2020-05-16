098e34a968d0bcb3852680657fd451aaa7a8320c
Andre Marques <andrefilipefmarques@gmail.com>
2020-05-16 14:35:37 +0100

    Sim 2

---
+++ Guide_04/Simulator2.m
@@ -0,0 +1,142 @@
+function [PL , PLV , APD , APDV , MPD , MPDV , TT] = Simulator2(lambda,n,C,f,P)
+% INPUT PARAMETERS:
+%  lambda - packet rate (packets/sec)
+%  C      - link bandwidth (Mbps)
+%  f      - queue size (Bytes)
+%  P      - number of packets (stopping criterium)
+% OUTPUT PARAMETERS:
+%  PL   - packet loss (%)
+%  APD  - average packet delay (milliseconds)
+%  MPD  - maximum packet delay (milliseconds)
+%  TT   - transmitted throughput (Mbps)
+
+%Events:
+ARRIVAL= 0;       % Arrival of a packet            
+DEPARTURE= 1;     % Departure of a packet
+DATA = 0;
+VOIP = 1;
+%State variables:
+State = 0;          % 0 - connection free; 1 - connection bysy
+QueueOccupation= 0; % Occupation of the queue (in Bytes)
+Queue= [];          % Size and arriving time instant of each packet in the queue
+%Statistical Counters:
+TotalPackets= 0;       % No. of packets arrived to the system
+TotalPacketsV= 0;       % No. of packets arrived to the system
+LostPackets= 0;        % No. of packets dropped due to buffer overflow
+LostPacketsV= 0;        % No. of packets dropped due to buffer overflow
+TransmittedPackets= 0; % No. of transmitted packets
+TransmittedPacketsV= 0; % No. of transmitted packets
+TransmittedBytes= 0;   % Sum of the Bytes of transmitted packets
+Delays= 0;             % Sum of the delays of transmitted packets
+DelaysV= 0;             % Sum of the delays of transmitted packets
+MaxDelay= 0;           % Maximum delay among all transmitted packets
+MaxDelayV= 0;           % Maximum delay among all transmitted packets
+%Auxiliary variables:
+% Initializing the simulation clock:
+Clock= 0;
+
+% Initializing the List of Events with the first ARRIVAL:
+EventList = [ARRIVAL , Clock + exprnd(1/lambda) , GeneratePacketSize() , 0, DATA];
+for i=1:n
+    EventList = [EventList; ARRIVAL , Clock + 0.02*rand() , GeneratePacketSizeVOIP() , 0, VOIP];
+end
+%Similation loop:
+while TransmittedPackets+TransmittedPacketsV<P               % Stopping criterium
+    EventList= sortrows(EventList,2);    % Order EentList by time
+    Event= EventList(1,1);               % Get first event and 
+    Clock= EventList(1,2);               %   and
+    PacketSize= EventList(1,3);          %   associated
+    ArrivalInstant= EventList(1,4);      %   parameters.
+    Type=EventList(1,5 );
+    EventList(1,:)= [];                  % Eliminate first event
+    switch Event
+        case ARRIVAL                     % If first event is an ARRIVAL
+            switch Type
+                case DATA
+                    TotalPackets= TotalPackets+1;
+                    EventList = [EventList; ARRIVAL , Clock + exprnd(1/lambda) , GeneratePacketSize() , 0, DATA];
+                    if State==0
+                        State= 1;
+                        EventList = [EventList; DEPARTURE , Clock + 8*PacketSize/(C*10^6) , PacketSize , Clock, DATA];
+                    else
+                        if QueueOccupation + PacketSize <= f
+                            Queue= [Queue;PacketSize , Clock, Type];
+                            QueueOccupation= QueueOccupation + PacketSize;
+                        else
+                            LostPackets= LostPackets + 1;
+                        end
+                    end
+                case VOIP
+                    TotalPacketsV= TotalPacketsV+1;
+                    EventList = [EventList; ARRIVAL , Clock + 0.016+0.008*rand() , GeneratePacketSizeVOIP() , 0, VOIP];
+                    if State==0
+                        State= 1;
+                        EventList = [EventList; DEPARTURE , Clock +  8*PacketSize/(C*10^6) , PacketSize , Clock, VOIP];
+                    else
+                        if QueueOccupation + PacketSize <= f
+                            Queue= [Queue;PacketSize , Clock, Type];
+                            QueueOccupation= QueueOccupation + PacketSize;
+                        else
+                            LostPacketsV= LostPacketsV + 1;
+                        end
+                    end
+            end
+        case DEPARTURE                     % If first event is a DEPARTURE
+            switch Type
+                case DATA
+                    TransmittedBytes= TransmittedBytes + PacketSize;
+                    Delays= Delays + (Clock - ArrivalInstant);
+                    if Clock - ArrivalInstant > MaxDelay
+                        MaxDelay= Clock - ArrivalInstant;
+                    end
+                    TransmittedPackets= TransmittedPackets + 1;
+                    if QueueOccupation > 0
+                        EventList = [EventList; DEPARTURE , Clock + 8*Queue(1,1)/(C*10^6) , Queue(1,1) , Queue(1,2), Queue(1,3)];
+                        QueueOccupation= QueueOccupation - Queue(1,1);
+                        Queue(1,:)= [];
+                    else
+                        State= 0;
+                    end
+                case VOIP
+                    TransmittedBytes= TransmittedBytes + PacketSize;
+                    DelaysV= DelaysV + (Clock - ArrivalInstant);
+                    if Clock - ArrivalInstant > MaxDelayV
+                        MaxDelayV= Clock - ArrivalInstant;
+                    end
+                    TransmittedPacketsV= TransmittedPacketsV + 1;
+                    if QueueOccupation > 0
+                        EventList = [EventList; DEPARTURE , Clock + 8*Queue(1,1)/(C*10^6) , Queue(1,1) , Queue(1,2), Queue(1,3)];
+                        QueueOccupation= QueueOccupation - Queue(1,1);
+                        Queue(1,:)= [];
+                    else
+                        State= 0;
+                    end
+                    end
+    end
+end
+
+%Performance parameters determination:
+PL= 100*LostPackets/TotalPackets;      % in %
+PLV= 100*LostPacketsV/TotalPacketsV;      % in %
+APD= 1000*Delays/TransmittedPackets;   % in milliseconds
+APDV= 1000*DelaysV/TransmittedPacketsV;   % in milliseconds
+MPD= 1000*MaxDelay;                    % in milliseconds
+MPDV= 1000*MaxDelayV;                    % in milliseconds
+TT= 10^(-6)*TransmittedBytes*8/Clock;  % in Mbps
+
+end
+
+function out= GeneratePacketSize()
+    aux= rand();
+    if aux <= 0.16
+        out= 64;
+    elseif aux >= 0.78
+        out= 1518;
+    else
+        out = randi([65 1517]);
+    end
+end
+
+function out= GeneratePacketSizeVOIP()
+    out = randi([110 130]);
+end
--- Guide_04/mg1.m
+++ Guide_04/mg1.m
@@ -6,15 +6,21 @@ function APD = mg1(lambda,C)
 
 p64 = 0.16;
 p1518=0.22;
-pEq = (1-0.22-0.16);
+pResto = (1-p1518-p64);
+pIndividual = pResto/(1517-65+1);
 
 
 t64 = 64/(125000*C);
 t1518 = 1518/(125000*C);
 tEq = ((65+1517)/2)/(125000*C);
 
-es = p64*t64+p1518*t1518+pEq*tEq;
-es2 = p64*t64^2+p1518*t1518^2+pEq*tEq^2;
+es = p64*t64+p1518*t1518+pResto*tEq;
+es2 = p64*t64^2 + p1518*t1518^2;
+
+for tamanho=65:1517
+    tIndividual = tamanho/(125000*C);
+    es2 = es2 + pIndividual*tIndividual^2;
+end
 
 APD=((lambda*es2)/(2*(1-lambda*es))) + es;
 APD = APD *1000;
